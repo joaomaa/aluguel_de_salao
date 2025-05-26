f4e08be2ee8d0f557439a15867cadcbeb521db95
João Antonio <joaoantoniomaa@gmail.com>
2025-05-26 15:00:15 -0300

    Concertando bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
+import './index.css'
 import React from 'react';
-import './index.css';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
--- src/pages/SingPage/Register.jsx
+++ src/pages/SingPage/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
 
 export default function Register() {
