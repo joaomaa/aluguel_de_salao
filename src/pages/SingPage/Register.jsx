13736c99a2b0e2d26785e444fc55f8a53f1b1f44
João Antonio <joaoantoniomaa@gmail.com>
2025-05-26 23:27:01 -0300

    Melhorando usabilidade

--- src/pages/SingPage/Register.jsx
+++ src/pages/SingPage/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
 
 export default function Register() {
